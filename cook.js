5e8df0f7c712319c512f1d3a611dc0a961217f47
Lisa Fedorenko <lisafedorenko@gmail.com>
2016-05-01 19:48:29 +1000

    first javascipt addition

---
+++ cook.js
@@ -0,0 +1,29 @@
+//<script language="JavaScript" type="text/javascript">
+// test button
+function myFunction() {
+document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+// selects multiple items
+function getSelectValues(select) {
+var result = [];
+var options = select && select.options;
+var opt;
+
+for (var i=0, iLen=options.length; i<iLen; i++) {
+  opt = options[i];
+  if (opt.selected) {
+  result.push(opt.value || opt.text);
+  }
+}
+alert(result);
+return result;
+}
+
+// selects utensils 
+function utensils() {
+var selected = document.getElementById('items');
+var myitems = new Array();
+var selObj = document.getElementById('
+}
+
