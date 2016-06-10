b2169b9cc0c4f756858a117f473148be7a857a26
Lisa Fedorenko <lisafedorenko@gmail.com>
2016-06-10 13:13:11 +1000

    wrote tools/ methods for all proteins

--- objects.js
+++ objects.js
@@ -1,14 +1,15 @@
 // Proteins
-var poachedEgg = {tools:"microwave", method:"place in mug, cover the top with a paper towel, microwave for 30 seconds, stir and microwave another 20 secs if needed"}
-var friedEgg = {tools:"microwave"}
-var scrambledEgg = {tools:"microwave"}
-var tofu = {tools:"sandwichPress"}
-var bbqChicken = {tools:""}
-var smokedSalmon = {tools:""}
-var leftOver = {tools:"microwave"}
-var beef = {tools:"sandwichPress"}
-var lamb = {tools:"sandwichPress"}
-var fish = {tools:"sandwichPress"}
+var poachedEgg = {tools: "microwave", method: "Place in mug, cover the top of the mug  with a paper towel. Microwave for 30 seconds, stir and microwave another 20 secs if needed"}
+var friedEgg = {tools:"sandwichPress", method: "Heat up sandwich press and line the base with Al foil. Add some oil to avoid sticking. Crack an egg and fry until cooked through. Remove foil and slide egg onto plate from foil. Cool the press and throw out the al foil"}
+var scrambledEgg = {tools: "microwave", method: "Crack two eggs in a bowl. Add a splash of water. Beat thoroughly with a fork. Place in the microwave for 30 seconds, remove and stir, place in microwave another 20 seconds if necessary"}
+var tofu = {tools: "sandwichPress", method: "Heat up sandwich press and line the base and top with Al foil. Add some oil to avoid sticking. Slice up the tofu into thin strips. Place the tofu strips onto the foil and close the press. Tofu should be cooked and golden within 2-3 mins. Remove the tofu when at your liking, use this as an opportunity to grill any vegetables you would like by chopping up some mushrooms/ zuchini/ capsicum and placing on the sandwich press for 30 seconds, turning then removing."}
+var bbqChicken = {tools: "", method: "Either serve whole on your carb or pull into small bite size pieces"}
+var tunaCan = {tools: "", method: "Remove from packet and mix in"}
+var smokedSalmon = {tools: "", method: "Remove from packet serve the slices"}
+var leftOver = {tools: "microwave", method: "Heat in microwave for 1-2 minutes, serve on top"}
+var steak = {tools: "sandwichPress", method: "Heat up sandwich press and line the base and top with Al foil. Dress the steak with salt/ pepper/ chilli (whatever you keep on hand and feel in the mood for). Then rub both sides in oil. Place into the press and close the lid. Steak should be cooked to medium rare within c3 minutes (it cooks from both sides so is very fast). Remove the steak, make a small incision to ensure it is cooked through to your liking. Serve either whole or cut into strips. Use this as an opportunity to grill any vegetables you would like by chopping up some mushrooms/ zuchini/ capsicum and placing on the sandwich press for 30 seconds, turning then removing."}}
+var lamb = {tools: "sandwichPress", method: "Heat up sandwich press and line the base and top with Al foil. Dress the lamb with salt/ pepper/ chilli (whatever you keep on hand and feel in the mood for). Then rub both sides in oil. Place into the press and close the lid. Lamb should be cooked to medium rare within c3 minutes (it cooks from both sides so is very fast). Remove the lamb, make a small incision to ensure it is cooked through to your liking. Serve either whole or cut into strips. Use this as an opportunity to grill any vegetables you would like by chopping up some mushrooms/ zuchini/ capsicum and placing on the sandwich press for 30 seconds, turning then removing."}}
+var fish = {tools: "sandwichPress", method: "Heat up sandwich press. Take your fish fillet and rub both sides with oil, dress with salt pepper/ spices to your liking. Lay salmon onto a sheet of al foil skin side down. Cut lemon slices and lay over the fish. Wrap up the al foil from both sides then seal the edges to create a parcel. Place this in the sandwich press. Fish will be cooked rare within 3 minutes, and cooked thoroughly within 6 minutes. Remove at a point to suit your tastes, unwrap the salmon and serve."}}
 
 // Carbs 
 var salad = {tools:""}
