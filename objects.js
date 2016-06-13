f7082d2b1968923e51eebaf36a24dbf7fa5a4ead
Lisa Fedorenko <lisafedorenko@gmail.com>
2016-06-13 18:15:11 +1000

    using more object properties

--- objects.js
+++ objects.js
@@ -1,12 +1,87 @@
+function Food(name, type, tools, method, image) {
+  this.name = name;
+  this.type = type;
+  this.tools = tools;
+  this.method = method;
+  this.image = image;
+};
+
+Food.prototype.eat = function() {
+  alert("Yum! I hoped you used a " + this.tools);
+};
+
 // Proteins
-var poachedEgg = {tools: "microwave", method: "Place in mug, cover the top of the mug  with a paper towel. Microwave for 30 seconds, stir and microwave another 20 secs if needed"}
-var friedEgg = {tools:"sandwichPress", method: "Heat up sandwich press and line the base with Al foil. Add some oil to avoid sticking. Crack an egg and fry until cooked through. Remove foil and slide egg onto plate from foil. Cool the press and throw out the al foil"}
-var scrambledEgg = {tools: "microwave", method: "Crack two eggs in a bowl. Add a splash of water. Beat thoroughly with a fork. Place in the microwave for 30 seconds, remove and stir, place in microwave another 20 seconds if necessary"}
-var tofu = {tools: "sandwichPress", method: "Heat up sandwich press and line the base and top with Al foil. Add some oil to avoid sticking. Slice up the tofu into thin strips. Place the tofu strips onto the foil and close the press. Tofu should be cooked and golden within 2-3 mins. Remove the tofu when at your liking, use this as an opportunity to grill any vegetables you would like by chopping up some mushrooms/ zuchini/ capsicum and placing on the sandwich press for 30 seconds, turning then removing."}
-var bbqChicken = {tools: "", method: "Either serve whole on your carb or pull into small bite size pieces"}
-var tunaCan = {tools: "", method: "Remove from packet and mix in"}
-var smokedSalmon = {tools: "", method: "Remove from packet serve the slices"}
-var leftOver = {tools: "microwave", method: "Heat in microwave for 1-2 minutes, serve on top"}
+var foods = [
+  new Food("poachedEgg", "protein"),
+]
+
+Recipe = function(ingredients) {
+  this.ingredients = ingredients;
+};
+
+function Pantry() {
+  this.contents = [];
+};
+
+Pantry.prototype.addFoods = function(foods) {
+  for (var food in foods) {
+    this.contents.append(food);
+  }
+};
+
+Pantry.prototype.getType = function(type) {
+  var results = [];
+  for (var food in this.contents) {
+    if (food.type == type) {
+     results.append(food);
+    }
+  }
+  return results;
+};
+
+Pantry.prototype.chooseRandom = function(type) {
+  var all = this.getType(type);
+  return all[0];
+};
+
+// 
+pantry.getType('protein')
+
+for (var food in foods) {
+  food.eat();
+}
+
+var poachedEgg = {
+	tools: "microwave", 
+	method: "Place in mug, cover the top of the mug  with a paper towel. Microwave for 30 seconds, stir and microwave another 20 secs if needed",
+	img: "INSERT_IMG_REF_HERE"
+}
+var friedEgg = {
+	tools:"sandwichPress", 
+	method: "Heat up sandwich press and line the base with Al foil. Add some oil to avoid sticking. Crack an egg and fry until cooked through. Remove foil and slide egg onto plate from foil. Cool the press and throw out the al foil"
+}
+var scrambledEgg = {
+	tools: "microwave", 
+	method: "Crack two eggs in a bowl. Add a splash of water. Beat thoroughly with a fork. Place in the microwave for 30 seconds, remove and stir, place in microwave another 20 seconds if necessary"
+}
+var tofu = {
+	tools: "sandwichPress", 
+	method: "Heat up sandwich press and line the base and top with Al foil. Add some oil to avoid sticking. Slice up the tofu into thin strips. Place the tofu strips onto the foil and close the press. Tofu should be cooked and golden within 2-3 mins. Remove the tofu when at your liking, use this as an opportunity to grill any vegetables you would like by chopping up some mushrooms/ zuchini/ capsicum and placing on the sandwich press for 30 seconds, turning then removing."
+}
+var bbqChicken = {
+	tools: "", 
+	method: "Either serve whole on your carb or pull into small bite size pieces"
+}
+var tunaCan = {
+	tools: "", 
+	method: "Remove from packet and mix in"
+}
+var smokedSalmon = {
+	tools: "", 
+	method: "Remove from packet serve the slices"
+}
+var leftOver = {tools: "microwave", method: "Heat in microwave for 1-2 minutes, serve on top"
+}
 var steak = {tools: "sandwichPress", method: "Heat up sandwich press and line the base and top with Al foil. Dress the steak with salt/ pepper/ chilli (whatever you keep on hand and feel in the mood for). Then rub both sides in oil. Place into the press and close the lid. Steak should be cooked to medium rare within c3 minutes (it cooks from both sides so is very fast). Remove the steak, make a small incision to ensure it is cooked through to your liking. Serve either whole or cut into strips. Use this as an opportunity to grill any vegetables you would like by chopping up some mushrooms/ zuchini/ capsicum and placing on the sandwich press for 30 seconds, turning then removing."}}
 var lamb = {tools: "sandwichPress", method: "Heat up sandwich press and line the base and top with Al foil. Dress the lamb with salt/ pepper/ chilli (whatever you keep on hand and feel in the mood for). Then rub both sides in oil. Place into the press and close the lid. Lamb should be cooked to medium rare within c3 minutes (it cooks from both sides so is very fast). Remove the lamb, make a small incision to ensure it is cooked through to your liking. Serve either whole or cut into strips. Use this as an opportunity to grill any vegetables you would like by chopping up some mushrooms/ zuchini/ capsicum and placing on the sandwich press for 30 seconds, turning then removing."}}
 var fish = {tools: "sandwichPress", method: "Heat up sandwich press. Take your fish fillet and rub both sides with oil, dress with salt pepper/ spices to your liking. Lay salmon onto a sheet of al foil skin side down. Cut lemon slices and lay over the fish. Wrap up the al foil from both sides then seal the edges to create a parcel. Place this in the sandwich press. Fish will be cooked rare within 3 minutes, and cooked thoroughly within 6 minutes. Remove at a point to suit your tastes, unwrap the salmon and serve."}}
